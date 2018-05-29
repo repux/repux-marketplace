6ec549db2eb2a9bcd2f8a07d70caf8182baca9db
Dawid Rashid <dawid.rashid@workhq.com>
2018-05-29 15:52:19 +0200

    fix(*): add missing environment.prod.ts to production builds

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 import { environment as devEnvironment } from './environment.base';
 
 export const environment = Object.assign({}, devEnvironment);
-environment.production = true;
+environment.production = false;
