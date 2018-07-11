0645467f0e358e1c6cdd8b174acdcab134f9f2bc
Krzysztof Duralek <krzysztof.duralek@workhq.com>
2018-07-11 12:58:13 +0200

    fix(details): Back to l list link should take user to a previous page

--- src/app/marketplace/marketplace-product-details.component.ts
+++ src/app/marketplace/marketplace-product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import { DataProductListService } from '../services/data-product-list.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs/internal/Observable';
 import { map, pluck } from 'rxjs/operators';
 
@@ -12,7 +13,11 @@ import { map, pluck } from 'rxjs/operators';
 export class MarketplaceProductDetailsComponent implements OnInit {
   product$: Observable<any>;
 
-  constructor(private activeRoute: ActivatedRoute, private dataProductListService: DataProductListService) {
+  constructor(
+    private router: Router,
+    private location: Location,
+    private activeRoute: ActivatedRoute,
+    private dataProductListService: DataProductListService) {
   }
 
   ngOnInit() {
@@ -27,5 +32,13 @@ export class MarketplaceProductDetailsComponent implements OnInit {
       map(obj => obj[ 0 ]),
       pluck('source')
     );
+  }
+
+  goBack() {
+    if (<any>document.referrer !== '') {
+      this.location.back();
+    } else {
+      this.router.navigateByUrl('/marketplace');
+    }
   }
 }
