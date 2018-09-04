017f1045193e049b30d16b3ba2a2f0d012bb2732
Dawid Rashid <drashid@repux.io>
2018-09-04 10:40:52 +0200

    feat: [REPUX-749] changes to the push notifications

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,11 +2,29 @@ self.addEventListener('install', () => {
     console.log('[sw]', 'Your ServiceWorker is installed');
 });
 
-self.addEventListener('push', ev => {
-    console.log('[sw]', 'pushed!!', ev.data.json());
-    const {title, msg, icon} = ev.data.json();
+self.addEventListener('push', event => {
+    console.log('[sw]', 'pushed!!', event.data.json());
+    const { title, msg, actions, data } = event.data.json();
+
     self.registration.showNotification(title, {
         body: msg,
-        icon: icon,
+        icon: '/assets/repux-icon-512x512.png',
+        actions: actions,
+        data: data
     });
+});
+
+self.addEventListener('notificationclick', event => {
+    const { productAddress, type } = event.notification.data;
+
+    let url = '/marketplace';
+
+    if (type === 'finalise') {
+        url = '/marketplace/details/' + productAddress;
+    } else if (type === 'purchase') {
+        url = '/notifications';
+    }
+
+    const promiseChain = clients.openWindow(url);
+    event.waitUntil(promiseChain);
 });
