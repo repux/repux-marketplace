4317f910ee0f83355f9ac36a6dd7a718c0a5c277
Krzysztof Duralek <krzysztof.duralek@workhq.com>
2018-09-11 13:57:16 +0200

    Rename seller rating section & remove seller rating stars

--- src/app/marketplace/marketplace-rating/marketplace-rating.component.ts
+++ src/app/marketplace/marketplace-rating/marketplace-rating.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import BigNumber from 'bignumber.js';
 import { DataProductOrder } from '../../shared/models/data-product-order';
 
 @Component({
@@ -9,21 +8,9 @@ import { DataProductOrder } from '../../shared/models/data-product-order';
   templateUrl: './marketplace-rating.component.html'
 })
 export class MarketplaceRatingComponent {
-  @Input() rating = new BigNumber(0);
   @Input() orders: DataProductOrder[];
   @Input() minRate = environment.repux.minProductRate;
   @Input() maxRate = environment.repux.maxProductRate;
-
-  get availableRates() {
-    return Array.from(Array(this.maxRate - this.minRate + 1).keys()).map(key => key + this.minRate);
-  }
-
-  get percentageRating(): number {
-    return this.rating
-      .div(this.maxRate)
-      .times(100)
-      .toNumber();
-  }
 
   get ratingSnapshot(): number[] {
     if (!this.orders) {
