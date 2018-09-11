b1e02956d1bbab4a18cb5f72ed4184f55e5da7a1
Dawid Rashid <drashid@repux.io>
2018-09-11 16:15:07 +0200

    fix: error related with animations on production environment

--- src/app/shared/animations/folding.animation.ts
+++ src/app/shared/animations/folding.animation.ts
@@ -1,15 +1,17 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, AnimationTriggerMetadata, state, style, transition, trigger } from '@angular/animations';
 
-export default trigger('visibilityChanged', [
-  state('true', style({
-    overflow: 'hidden',
-    height: '*'
-  })),
-  state('false', style({
-    opacity: '0',
-    overflow: 'hidden',
-    height: '0'
-  })),
-  transition('1 => 0', animate('300ms ease-in-out')),
-  transition('0 => 1', animate('300ms ease-in-out'))
-]);
+export function foldingAnimation(triggerName: string): AnimationTriggerMetadata {
+  return trigger(triggerName, [
+    state('true', style({
+      overflow: 'hidden',
+      height: '*'
+    })),
+    state('false', style({
+      opacity: '0',
+      overflow: 'hidden',
+      height: '0'
+    })),
+    transition('1 => 0', animate('300ms ease-in-out')),
+    transition('0 => 1', animate('300ms ease-in-out'))
+  ]);
+}
