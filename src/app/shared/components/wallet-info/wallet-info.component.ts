2c4dbfc61f496699fe9ebd60d4f1a0965fd321a2
Krzysztof Duralek <krzysztof.duralek@workhq.com>
2018-09-13 08:13:47 +0200

    feat: Move faucet interface to marketplace modal

--- src/app/shared/components/wallet-info/wallet-info.component.ts
+++ src/app/shared/components/wallet-info/wallet-info.component.ts
@@ -3,7 +3,8 @@ import Wallet from '../../models/wallet';
 import { WalletService } from '../../../services/wallet.service';
 import { Observable } from 'rxjs/internal/Observable';
 import BigNumber from 'bignumber.js';
-import { environment } from '../../../../environments/environment';
+import { IssueDemoTokensComponent } from '../issue-demo-tokens/issue-demo-tokens.component';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-wallet-info',
@@ -13,14 +14,18 @@ import { environment } from '../../../../environments/environment';
 export class WalletInfoComponent implements OnInit {
   wallet$: Observable<Wallet>;
   balance$: Observable<BigNumber>;
-  faucetUrl: String;
 
-  constructor(private walletService: WalletService) {
+  constructor(
+    private walletService: WalletService,
+    private dialog: MatDialog) {
   }
 
   ngOnInit(): void {
     this.wallet$ = this.walletService.getWallet();
     this.balance$ = this.walletService.getBalance();
-    this.faucetUrl = environment.faucetUrl;
+  }
+
+  openDemoTokensIssueDialog() {
+    this.dialog.open(IssueDemoTokensComponent);
   }
 }
